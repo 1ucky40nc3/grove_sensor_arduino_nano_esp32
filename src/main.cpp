7e3a676f861d86c530fcf2739e5d7b6033e46a4e
1ucky40nce3 <louisnwendler@gmail.com>
2025-04-13 20:06:34 +0200

    change data types

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #endif
 
 HM330X sensor;
-u8 buf[30];
+uint8_t buf[30];
 
 const char *str[] = {
     "sensor num: ",
@@ -21,7 +21,7 @@ const char *str[] = {
     "PM10 concentration(Atmospheric environment,unit:ug/m3): ",
 };
 
-err_t print_result(const char *str, u16 value)
+err_t print_result(const char *str, uint16_t value)
 {
   if (NULL == str)
     return ERROR_PARAM;
@@ -31,20 +31,20 @@ err_t print_result(const char *str, u16 value)
 }
 
 /*parse buf with 29 u8-data*/
-err_t parse_result(u8 *data)
+err_t parse_result(uint8_t *data)
 {
-  u16 value = 0;
+  uint16_t value = 0;
   err_t NO_ERROR;
   if (NULL == data)
     return ERROR_PARAM;
   for (int i = 1; i < 8; i++)
   {
-    value = (u16)data[i * 2] << 8 | data[i * 2 + 1];
+    value = (uint16_t)data[i * 2] << 8 | data[i * 2 + 1];
     print_result(str[i - 1], value);
   }
 }
 
-err_t parse_result_value(u8 *data)
+err_t parse_result_value(uint8_t *data)
 {
   if (NULL == data)
     return ERROR_PARAM;
@@ -57,7 +57,7 @@ err_t parse_result_value(u8 *data)
       SERIAL.println(" ");
     }
   }
-  u8 sum = 0;
+  uint8_t sum = 0;
   for (int i = 0; i < 28; i++)
   {
     sum += data[i];
